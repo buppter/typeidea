a5c112c1ce4d6b3d2b4af054736f03e8d32abfbe
buppter <shixintian@bupt.edu.cn>
2019-12-05 12:07:34 +0800

    init

---
+++ typeidea/typeidea/settings/__init__.py
@@ -0,0 +1,4 @@
+"""
+author: buppter
+datetime: 2019/12/4 8:29 下午
+"""
---
+++ typeidea/typeidea/settings/dev.py
@@ -0,0 +1,14 @@
+"""
+author: buppter
+datetime: 2019/12/4 8:30 下午
+"""
+from .base import *
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
