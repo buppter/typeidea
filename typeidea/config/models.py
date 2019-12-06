80348a65500c889d8c347d1e5078abc766eb5ab6
buppter <shixintian@aliyun.com>
2019-12-06 16:28:41 +0800

    :bug: 修复 BUG

--- typeidea/config/models.py
+++ typeidea/config/models.py
@@ -35,7 +35,7 @@ class SideBar(models.Model):
     title = models.CharField(max_length=50, verbose_name="标题")
     display_type = models.PositiveIntegerField(default=1, choices=SIDE_TYPE, verbose_name="展示类型")
     content = models.CharField(max_length=500, blank=True, verbose_name="内容", help_text="如果设置的不是 HTML 类型，可为空")
-    status = models.PositiveIntegerField(default=Status.STATUS_NORMAL, choices=Status.STATUS_ITEMS, verbose_name="状态")
+    status = models.PositiveIntegerField(default=STATUS_SHOW, choices=STATUS_ITEMS, verbose_name="状态")
     owner = models.ForeignKey(User, verbose_name="作者")
     create_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
 
