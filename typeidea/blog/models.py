4b1f7912667454a6838f5b4997c7e2d5a0575ee2
buppter <shixintian@aliyun.com>
2019-12-07 18:09:42 +0800

    :construction: 继续完善代码

--- typeidea/blog/models.py
+++ typeidea/blog/models.py
@@ -77,6 +77,8 @@ class Post(models.Model):
     category = models.ForeignKey(Category, verbose_name="分类")
     tag = models.ManyToManyField(Tag, verbose_name="标签")
     owner = models.ForeignKey(User, verbose_name="作者")
+    pv = models.PositiveIntegerField(default=1)
+    uv = models.PositiveIntegerField(default=1)
     create_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
 
     @staticmethod
@@ -105,6 +107,10 @@ class Post(models.Model):
     def latest_posts(cls):
         queryset = cls.objects.filter(status=cls.STATUS_NORMAL)
         return queryset
+
+    @classmethod
+    def hot_posts(cls):
+        return cls.objects.filter(status=cls.STATUS_NORMAL).order_by("-pv").only("id", "title")
 
     class Meta:
         verbose_name = verbose_name_plural = "文章"
--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -21,11 +21,11 @@ from blog.views import post_list, post_detail
 from config.views import links
 
 urlpatterns = [
-    url(r'^super_admin/', admin.site.urls),
-    url(r'^admin/', custom_site.urls),
-    url(r'^tag/(?P<tag_id>\d+)/$', post_list),
-    url(r'^post/(?P<post_id>\d+).html$', post_detail),
-    url(r'links/$', links),
-    url(r"^$", post_list),
-    url(r'^category/(?P<category_id>\d+)/$', post_list),
+    url(r'^super_admin/', admin.site.urls, name="super_admin"),
+    url(r'^admin/', custom_site.urls, name="admin"),
+    url(r'^tag/(?P<tag_id>\d+)/$', post_list, name="tag_list"),
+    url(r'^post/(?P<post_id>\d+).html$', post_detail, name="post_detail"),
+    url(r'^links/$', links, name="links"),
+    url(r"^$", post_list, name="index"),
+    url(r'^category/(?P<category_id>\d+)/$', post_list, name="category_list"),
 ]
