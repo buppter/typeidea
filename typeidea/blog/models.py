bf359583a58448d3ca2eadc8eb9958671de0802c
buppter <shixintian@aliyun.com>
2019-12-09 16:26:33 +0800

    :construction: 完成 Markdown 渲染和作者详情页

--- typeidea/blog/models.py
+++ typeidea/blog/models.py
@@ -1,5 +1,7 @@
+import mistune
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.functional import cached_property
 
 
 class Category(models.Model):
@@ -73,6 +75,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255, verbose_name="标题")
     desc = models.CharField(max_length=1024, blank=True, verbose_name="摘要")
     content = models.TextField(verbose_name="正文", help_text="正文必须为 MarkDown 格式")
+    content_html = models.TextField(verbose_name="正文 HTML 代码", blank=True, editable=False)
     status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name="状态")
     category = models.ForeignKey(Category, verbose_name="分类")
     tag = models.ManyToManyField(Tag, verbose_name="标签")
@@ -80,6 +83,14 @@ class Post(models.Model):
     pv = models.PositiveIntegerField(default=1)
     uv = models.PositiveIntegerField(default=1)
     create_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+
+    def save(self, *args, **kwargs):
+        self.content_html = mistune.markdown(self.content)
+        super().save(*args, **kwargs)
+
+    @cached_property
+    def tags(self):
+        return ",".join(self.tag.values_list('name', flat=True))
 
     @staticmethod
     def get_by_tag(tag_id):
