c60db58a0dcfffb9fc05b20a9d29ac4bff5c034f
buppter <shixintian@aliyun.com>
2019-12-09 21:57:19 +0800

    :construction: 添加 django-debug-toolbar

--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -13,7 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+import debug_toolbar
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.sitemaps import views as sitemap_views
 
@@ -23,6 +24,7 @@ from blog.sitemap import PostSitemap
 from blog.views import PostDetailView, IndexView, TagView, CategoryView, SearchView, AuthorView
 from comment.views import CommentView
 from config.views import LinkView
+from typeidea.settings import dev
 
 urlpatterns = [
     url(r'^super_admin/', admin.site.urls, name="super_admin"),
@@ -37,7 +39,7 @@ urlpatterns = [
     url(r'^author/(?P<owner_id>\d+)/$', AuthorView.as_view(), name="author"),
     url(r'^comment/$', CommentView.as_view(), name="comment"),
     url(r'^rss|feed/', LatestPostFeed(), name="rss"),
-    url(r'^sitemap\.xml$', sitemap_views.sitemap, {'sitemaps': {"posts": PostSitemap}})
+    url(r'^sitemap\.xml$', sitemap_views.sitemap, {'sitemaps': {"posts": PostSitemap}}),
 
     # function based view 时的各 URL 配置
     # url(r"^$", post_list, name="index"),
@@ -47,3 +49,13 @@ urlpatterns = [
     # url(r'^links/$', links, name="links"),
 
 ]
+if dev.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [
+                      # path('__debug__/', include(debug_toolbar.urls)),
+
+                      # For django versions before 2.0:
+                      url(r'^__debug__/', include(debug_toolbar.urls)),
+
+                  ] + urlpatterns
