611e21476ba76b555904d203425a6aca208927d4
buppter <shixintian@aliyun.com>
2019-12-09 16:25:16 +0800

    :construction: 完成站点统计功能

---
+++ typeidea/blog/middleware/__init__.py
@@ -0,0 +1,4 @@
+"""
+author: buppter
+datetime: 2019/12/9 15:46
+"""
--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,8 +1,14 @@
+from datetime import date
+
+from django.core.cache import cache
+from django.db.models import Q, F
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView, ListView
 
 # Create your views here.
 from blog.models import Tag, Post, Category
+from comment.forms import CommentForm
+from comment.models import Comment
 from config.models import SideBar
 
 
@@ -67,6 +73,32 @@ class PostDetailView(CommonViewMixin, DetailView):
     context_object_name = 'post'
     pk_url_kwarg = "post_id"
 
+    def get(self, request, *args, **kwargs):
+        response = super().get(request, *args, **kwargs)
+        self.handle_visited()
+        return response
+
+    def handle_visited(self):
+        increase_pv = False
+        increase_uv = False
+        uid = self.request.uid
+        pv_key = "pv:%s:%s" % (uid, self.request.path)
+        uv_key = "uv:%s:%s:%s" % (uid, str(date.today()), self.request.path)
+        if not cache.get(pv_key):
+            increase_pv = True
+            cache.set(pv_key, 1, 1 * 60)
+
+        if not cache.get(uv_key):
+            increase_uv = True
+            cache.set(uv_key, 1, 24 * 60 * 60)
+
+        if increase_uv and increase_pv:
+            Post.objects.filter(pk=self.object.id).update(pv=F('pv') + 1, uv=F('uv') + 1)
+        elif increase_pv:
+            Post.objects.filter(pk=self.object.id).update(pv=F('pv') + 1)
+        elif increase_uv:
+            Post.objects.filter(pk=self.object.id).update(uv=F('uv') + 1)
+
 
 class CategoryView(IndexView):
     def get_context_data(self, **kwargs):
@@ -100,3 +132,26 @@ class TagView(IndexView):
         queryset = super().get_queryset()
         tag_id = self.kwargs.get('tag_id')
         return queryset.filter(tag__id=tag_id)
+
+
+class SearchView(IndexView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context.update({
+            'keyword': self.request.GET.get('keyword', '')
+        })
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        keyword = self.request.GET.get('keyword')
+        if not keyword:
+            return queryset
+        return queryset.filter(Q(title__icontains=keyword) | Q(desc__icontains=keyword))
+
+
+class AuthorView(IndexView):
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        author_id = self.kwargs.get('owner_id')
+        return queryset.filter(owner_id=author_id)
